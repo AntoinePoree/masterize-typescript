10a3c82a4bfd4d97ff0774aaea2dc0ebb4ae2bc1
Antoine <antoine.poree@ebp.com>
2023-11-28 15:42:59 +0100

    feat: ajout d'un exemple de class

--- Contenu/III Les classes/3. Interfaces et Classes/script.ts
+++ Contenu/III Les classes/3. Interfaces et Classes/script.ts
@@ -1,31 +1,66 @@
 interface Country {
-    name: string;
-    population: number;
-    lang: string[] 
+  name: string;
+  population: number;
+  lang: string[];
 }
 
 class Norway implements Country {
-    constructor(
-        public name: string,
-        public population: number,
-        public lang: string[],
-        public Capital: string
-    ) {}
+  constructor(
+    public name: string,
+    public population: number,
+    public lang: string[],
+    public Capital: string
+  ) {}
 }
-const NorwayData = new Norway('Norway', 9, ["Norwegian"], "Oslo")
+const NorwayData = new Norway("Norway", 9, ["Norwegian"], "Oslo");
 console.log(NorwayData);
 
-
 class France implements Country {
-    constructor(
-        public name: string,
-        public population: number,
-        public lang: string[]
-    ) {}
+  constructor(
+    public name: string,
+    public population: number,
+    public lang: string[]
+  ) {}
 }
-const FranceData = new France('France', 70, ["Français", "Breton","Basque"])
+const FranceData = new France("France", 70, ["Français", "Breton", "Basque"]);
 console.log(FranceData);
 
 class Aquitaine extends France {}
 
-const AquitaineData = new Aquitaine("Aquitaine", 4, ["Français", "Basque","Gascon"])
+const AquitaineData = new Aquitaine("Aquitaine", 4, [
+  "Français",
+  "Basque",
+  "Gascon",
+]);
+
+/**
+ * EXEMPLE AVEC UN DTO POUR MAPPER DEPUIS UNE API
+ */
+
+export interface ISkillDTO {
+  id: string;
+  competence: string;
+  type: string;
+  note: number;
+}
+
+export interface ISkill {
+  id: string;
+  name: string;
+  type: string;
+  level: number;
+}
+
+export class Skill implements ISkill {
+  id: string;
+  name: string;
+  type: string;
+  level: number;
+
+  constructor(skill: ISkillDTO) {
+    this.id = skill.id;
+    this.name = skill.competence;
+    this.type = skill.type;
+    this.level = skill.note;
+  }
+}
